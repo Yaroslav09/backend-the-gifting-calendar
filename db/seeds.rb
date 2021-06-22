e36c3cb579a88228820b22d4702c20cbd7c0635d
Meg Gutshall <meghan.gutshall@gmail.com>
2021-06-22 18:43:42 -0400

    Add seed data for recipients

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Recipient.destroy_all
+
+Faker::Number.between(from: 6, to: 12).times do
+  Recipient.create!(
+    name: Faker::FunnyName.name,
+    mailing_address: Faker::Address.full_address,
+    preferences: Faker::Lorem.sentences(number: 1, supplemental: true)
+  )
+end
+
+puts "Created #{Recipient.all.count} recipients"
